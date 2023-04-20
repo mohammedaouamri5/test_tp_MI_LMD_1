7916df84054a4a8bf135c3ed2d288b45c207d7e6
mohammedaouamri5 <131393044+mohammedaouamri5@users.noreply.github.com>
2023-04-21 00:13:31 +0200

    Add files via upload

--- teesttp/script/UI.h
+++ teesttp/script/UI.h
@@ -1,7 +1,15 @@
 #pragma once
+#include "Agenda.h"
 
 #ifndef _UI_H_
 #define _UI_H_
-#include"Agenda.h"
+void Menu(int *MANAGER);
+void UI_ADD(List *L);
+void UI_MODIFIE(List *L);
+void UI_DELETE(List *L);
+void UI_AFFICHER(List *L);
+void UI_VIDEAR(List *L);
+
+
 
 #endif
